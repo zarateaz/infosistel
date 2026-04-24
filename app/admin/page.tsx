86fd60b8526d33370ccbd0f4f1e4d4e745d67c36
zarateaz <zaratepkm.3@gmail.com>
2026-04-23 21:20:52 -0500

    fix: restore correct map closure for table view in admin inventory

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -704,7 +704,7 @@ export default function AdminPage() {
                             </td>
                           </tr>
                         );
-                     ))}
+                     })}
 
                     </tbody>
                   </table>
